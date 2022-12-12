a113623bab37aac2aaf0ffdf88de2c44fa363a1f
Johnny <JohnnyLieu@yahoo.com>
2022-12-11 17:42:36 -0800

    done

--- main.py
+++ main.py
@@ -10,21 +10,17 @@ def rand_num():
 
     return rand_num1, rand_num2, rand_num3
 
-def check(a_score, b_score, num1, num2):
-    score = 0
-    user_selection = (input("Who has more followers? Type 'A' or 'B': ")).lower()
-    if user_selection == 'a':
-        if a_score > b_score:
-            score += 1
-            num1 = num2
+def ask(num1=rand_num()[0], num2=rand_num()[1], statement='', user_score=0):
+    score = user_score
 
-def ask(num1=rand_num()[0], num2=rand_num()[1], num3=rand_num()[2]):
     if num1 != num2:
+        os.system('cls')
         print(logo)
         a_name = data[num1]['name']
         a_score = data[num1]['follower_count']
         a_description = data[num1]['description']
         a_country = data[num1]['country']
+        print(statement)
         print(f"Compare A: {a_name}, a {a_description}, from {a_country}.")
 
         print(vs)
@@ -32,13 +28,31 @@ def ask(num1=rand_num()[0], num2=rand_num()[1], num3=rand_num()[2]):
         b_score = data[num2]['follower_count']
         b_description = data[num2]['description']
         b_country = data[num2]['country']
-        print(f"Compare A: {b_name}, a {b_description}, from {b_country}.")
+        print(f"Compare B: {b_name}, a {b_description}, from {b_country}.")
 
-    check(a_score, b_score, num1, num2)
+    user_selection = (input("Who has more followers? Type 'A' or 'B': ")).lower()
+
+    if user_selection == 'a':
+        if a_score > b_score:
+            score += 1
+            statement = f"You're right! Current score: {score}."
+            ask(num2, rand_num()[2], statement, score)
+        else:
+            os.system('cls')
+            print(f"{logo}\nSorry, that's wrong. Final score: {score}")
+    
+    if user_selection == 'b':
+        if b_score > a_score:
+            score += 1
+            statement = f"You're right! Current score: {score}."
+            ask(num2, rand_num()[2], statement, score)
+        else:
+            os.system('cls')
+            print(f"{logo}\nSorry, that's wrong. Final score: {score}")
+
 
 def main():
     if __name__ == "__main__":
-        os.system('cls')
         ask()
 
 main()
